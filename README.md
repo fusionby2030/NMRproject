7e023f3535ea3401e8631c6b1673cd75485ebae6
fusionby2030 <adamkit11@gmail.com>
2020-07-27 20:56:20 +0200

    Update README and remove nefarious line from create_graph util:

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # NMRproject
+
+## Current Overview [27.07.20]
+Below is an overview of the current structure of the package.
+
+- simpler_NMR_project/
+  - static/
+  - templates/
+    - graph_dash.html
+      - simple HTML rendering of the plotly graph as well as an input form and submit/reset buttons.
+  - sample_dashboard.py
+    - Single paged flask app
+    - create_graph function
+At the moment, this package has two functions.
+If you type in a number into the _New x value_ and _New y value_ fields then press submit, the graph is updated with the points you give it. Should you want to reset the graph, click reset, and all the data points disappear.
+There are various functions like zoom in/out, save as png, reset axes... that are located in the top right hand corner of the graph. 
+
+## Steps to reproduce
+
+In order to use this package, you must first make sure you have the proper dependencies.
+
+
+First clone/download/fork the repository.
+
+
+in the terminal, make a [virtual environment](https://docs.python.org/3/library/venv.html)
+
+
+Install dependencies from requirements.txt using
+`pip install -r /path/to/requirements.txt`
+
+in your terminal open up the directory if it is not already open and run the server via `python sample_dashboard.py`
+
+You should see various lines show up, but the most important is the one that says
+`Running on http:/XXX.X.X.X:XXXX` with numbers that may vary (for me it is `http://127.0.0.1:5000`). This the URL for the flask app is locally running on your computer, so open up your favourite browser and type in the URL.
--- simpler_NMR_project/sample_dashboard.py
+++ simpler_NMR_project/sample_dashboard.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request
+"""
+Single Paged Flask App
+graph_dash: renders the graph_dash.html page using create_graph
+            submit POST: update graph with new data points
+            reset POST: drop all data points and start with fresh empty list
 
+"""
 app=Flask(__name__)
-@app.route('/')
-def index():
-    bar = create_plot()
-    return render_template('index.html', plot=bar)
-@app.route('/graph_dash', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def graph_dash():
-    global x_list, y_list
+    global x_list, y_list #this is totally terrible and totally temporary
     if request.method == 'POST':
         if request.form['submit_button'] == 'populate':
             new_x = request.form.get('input_x')
@@ -19,32 +21,26 @@ def graph_dash():
             y_list = []
     scatter = create_graph(x_list, y_list)
     return render_template('graph_dash.html', plot=scatter)
+
 import plotly
 import plotly.graph_objs as go
 import pandas as pd
 import numpy as np
 import json
 
-def create_plot():
-    N = 40
-    x = np.linspace(0, 1, N)
-    y = np.random.randn(N)
-    df = pd.DataFrame({'x': x, 'y': y}) # creating a sample dataframe
-    data = [
-        go.Bar(
-            x=df['x'], # assign x as the dataframe column 'x'
-            y=df['y']
-        )
-    ]
-    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
-
-    return graphJSON
 def create_graph(x_list, y_list):
+    """
+    Creates a plotly JSON graph to be used for the graph_dash.html page
+    @param: x_list -> a list (should be floats)
+    @param: y_list -> a list
+    returns -> a JSON dump that can be interpreted with plotly style scripts
+    """
     df = pd.DataFrame({'x':x_list, 'y': y_list})
-    data = [go.Scatter(x=df['x'], y=df['y'], mode='markers'), go.Scatter(x=df['y'], y=df['x'], mode='markers')]
+    data = [go.Scatter(x=df['x'], y=df['y'], mode='markers')]
+    #TODO: ADD go.TangentLine
     graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
 
 if __name__=='__main__':
-    x_list = y_list = []
+    x_list = y_list = [] #once again this is temporary
     app.run(debug=True)
